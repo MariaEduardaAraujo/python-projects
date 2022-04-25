57676f9a5176eb17c600a5abd24de2fbca819bdd
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 20:15:27 -0300

    Update and rename Questão Sala - Aula de Programação.py to Questionário Aula de Programação.py

---
+++ Questionário Aula de Programação.py
@@ -0,0 +1,164 @@
+##Questão 1
+""" lado1= 7
+lado2= 3
+
+area=lado1*lado2
+print(area) """
+
+##Questão 2
+""" lado1=float(input('Digite a base: '))
+lado2=float(input('Digite a altura: '))
+
+area= lado1*lado2
+print(area) """
+
+##Questão 3
+""" a=float(input('Digite um número: '))
+b=float(input('Digite um outro número: '))
+
+print('A',a)
+print('B',b)
+
+print('Trocar!\n')
+
+temp=b
+b=a
+a=temp
+
+print('A',a)
+print('B',b) """
+
+##Questão 4
+""" n_carros=float(input('Digie o numero de carros vendidos: '))
+v_total=float(input('Valor total de vendas: '))
+
+fixo=float(input('Salário fixo: '))
+bonus=float(input('Bonus por vendas: '))
+salario= fixo + bonus*n_carros + 0.05*v_total
+
+##print('Salário deste mês: ', salario) """
+
+##Questão 5
+""" n1=float(input('Digite a primeira nota bimestral: '))
+n2=float(input('Digite a segunda nota bimestral: '))
+n3=float(input('Digite a terceira nota bimestral: '))
+n4=float(input('Digite a quarta nota bimestral: '))
+
+mf=(n1+n2+n3+n4)
+r=mf/4
+
+print('O resultado é: ', r) """
+
+##Questão 6
+""" metros=float(input('Digite a quantidade em metros: '))
+centimetros=metros/100
+
+print(metros, 'o valor em centimetros é', metros*100) """
+
+##Questão 7
+""" s_hora=float(input('Quanto você ganha por hora: '))
+h_trabalhadas=float(input('Quantas horas você trabalha: '))
+
+s_hora*h_trabalhadas*20
+r=s_hora*h_trabalhadas*20
+
+print(r) """
+
+##Questão 8
+""" temperatura_f=float(input('Digite a temperatura em FARENHEIT: '))
+
+vT=temperatura_f-32
+c=5*vT/9
+
+print(c) """
+
+##Questão 9
+""" temperatura_c=float(input('Digite a temperatura em CELSIUS: '))
+
+vT=temperatura_c*1.8
+f=vT+32
+
+print(f) """
+
+##Questão 10
+""" peixes_p=float(input('Digite o numero de peixes pescados: '))
+excesso= peixes_p - 50
+multa = 4.00
+
+resultado=excesso*4.00
+
+print('O valor da multa é: ', resultado) """
+
+##Questão 11
+""" tm=float(input('Digite o tamanho da area em metros quadrados: '))
+litros= tm/3
+c_lata= 18
+valor= 80
+
+latas=litros+c_lata
+r=latas*valor
+
+print('A quantidade de latas a serem usadas é de: ',latas)
+print('O preço a pagar será de: ', r) """
+
+##Questão 12
+""" t_arquivo=float(input('Tamanho do arquivo (MB): '))
+velocidade=float(input('Velocidade de internet (Mbps): '))
+
+print('Tempo aproximado: ', (t_arquivo/velocidade)*60) """
+
+##Questão 13
+""" salario_h=float(input('Digite o quanto você recebe por hora: '))
+horas_tr=float(input('Digite o número de horas trabalhadas neste mês: '))
+
+salariobruto=salario_h*horas_tr
+
+ir=(11/100*salariobruto)
+inss=(8/100*salariobruto)
+sin=(5/100*salariobruto)
+vt=ir+inss+sin
+sl=salariobruto-vt
+
+print('Seu salário bruto é de: ', salariobruto, '\n')
+print('Valor dos impostos: \n')
+print('Imposto de Renda: ', ir)
+print('INSS', inss)
+print('Sindicato', sin)
+print('Salário Líquido: ', sl) """
+
+##Questão 14
+##LATA DE TINTA
+
+##Questão 15
+""" sal_at = float(input('Salário atual: '))
+sal_au = sal_at*33/100
+
+print('Seu salário novo é de : R$', sal_au+sal_at)
+print('O aumento foi de: R$', sal_au) """
+
+##Questão 16
+""" prod1 = float(input('Preço do produto 1: '))
+prod2 = float(input('Preço do produto 2: '))
+prod3 = float(input('Preço do produto 3: '))
+
+qnt_prod1 = int(input('Quantidade de produtos vendidos: '))
+qnt_prod2 = int(input('Quantidade de produtos vendidos: '))
+qnt_prod3 = int(input('Quantidade de produtos vendidos: '))
+
+vl_tot = prod1*qnt_prod1, prod2*qnt_prod2, prod3*qnt_prod3
+
+print('Valor total da venda: R$', vl_tot)
+print('Número de itens vendidos: ', qnt_prod1, qnt_prod2, qnt_prod3) """
+
+##Questão 16.5
+""" a = int(input('Valor a: '))
+b = int(input('Valor b: '))
+c = int(input('Valor c: '))
+
+dlt = (b**2) - (4*a*c)
+
+print('X1: ', , 'X2: ', ) """
+
+##Questão 18
+##Questão 19
+##Questão 20
