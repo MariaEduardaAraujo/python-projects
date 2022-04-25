ea089d336ed1e22ab925a07fb7f5b2ad8b165b09
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 19:08:26 -0300

    Update Nota Válida.py

--- Nota Válida.py
+++ Nota Válida.py
@@ -1,8 +1,8 @@
-##nota = int(input("Digite uma nota entre 0 e 10: "))
-##
-##while nota >10 or nota < 0:
-##    print("Nota inválida")
-##    nota = int(input("Digite uma nota entre 0 e 10: "))
+nota = int(input("Digite uma nota entre 0 e 10: "))
+
+while nota >10 or nota < 0:
+    print("Nota inválida")
+    nota = int(input("Digite uma nota entre 0 e 10: "))
 
 nomeDeUsuario = str(input("Nome de Usuário: "))
 senha = str(input("Senha: "))
