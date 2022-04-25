cf8a2a021b9796a3eebc45761db82fb750e7b9cd
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 18:29:35 -0300

    Delete Data.py

--- Data.py
+++
@@ -1,7 +0,0 @@
-data = open("Base De Gastos.txt", "a")
-data.write("Gastos:\n ")
-data.close()
-
-#open and read the file after the appending:
-data = open("Base De Gastos.txt", "r")
-print(data.read())
