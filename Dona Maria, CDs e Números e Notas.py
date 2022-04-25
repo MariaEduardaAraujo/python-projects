a500e9e1bb8ebe54de15c0c111dff2e3ff82912d
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 19:07:17 -0300

    Update Dona Maria, CDs e Números e Notas.py

--- Dona Maria, CDs e Números e Notas.py
+++ Dona Maria, CDs e Números e Notas.py
@@ -1,57 +1,60 @@
-##prod = float(input('Digite um valor inicial (maior que zero): '))
-##soma = 0
-##
-##while prod>0:
-##    prod = float(input('Valor: '))
-##    soma = soma+prod
-##
-##print(soma)
-##
-##valores = int(input('Digite um valor inicial maior que zero: '))
-##soma = 0
-##
-##while valores>0:
-##    valores = float(input('Digite o valor: '))
-##    soma+= valores
-##
-##print(soma-valores)
-##
-##v = []
-##a = int(input(': '))
-##while a>0 and a<1000:
-##    v.append(a)
-##    a = int(input(': '))
-##    maior = max(v)
-##    menor = min(v)
-##    soma = max(v)+min(v)
-##print('Maior: ', maior, '\nMenor: ', menor, '\nSoma: ', soma)
-##
-##cds = []
-##quantidade = int(input('Quantidade de CDs: '))
-##
-##for i in range(0, quantidade):
-##    valorcd = int(input('Valor do CD: '))
-##    cds.append(valorcd)
-##
-##valortotal = sum(cds)
-##valormedio = valortotal/quantidade
-##print('Valor Médio: ', valormedio, 'Valor total: ', valortotal)
-##
-##alunos = [] 
-##for i in range(15):
-##    nota1 = eval(input('Nota 1: '))
-##    nota2 = eval(input('Nota 2: '))
-##    nota3 = eval(input('Nota 3: '))
-##    nota4 = eval(input('Nota 4: '))
-##    qtdfaltas = eval(input('Quantidade de faltas: '))
-##    media =(nota1*2+nota2*3+nota3*1+nota4*4)/10
-##    faltas = qtdfaltas*0.01
-##
-##    if media>6 and faltas<25:
-##        print('Aprovado')
-##        nome = input('Nome do aluno: ')
-##        alunos.append(nome)
-##    elif media<3 or faltas>25:
-##        print('Reprovado')
-##print(alunos)
+##Dona Maria
+""" prod = float(input('Digite um valor inicial (maior que zero): '))
+soma = 0
 
+while prod>0:
+  prod = float(input('Valor: '))
+  soma = soma+prod
+  print(soma)
+
+valores = int(input('Digite um valor inicial maior que zero: '))
+soma = 0
+
+while valores>0:
+  valores = float(input('Digite o valor: '))
+  soma+= valores
+
+print(soma-valores) """
+
+##Números
+"""v = []
+a = int(input('Digite um número de 0 a 1000: '))
+
+while a>0 and a<1000:
+  v.append(a)
+  a = int(input('Digite outro número de 0 a 1000: '))
+  maior = max(v)
+  menor = min(v)
+  soma = max(v)+min(v)
+print('Maior: ', maior, '\nMenor: ', menor, '\nSoma: ', soma)"""
+
+##CDS
+""" cds = []
+quantidade = int(input('Quantidade de CDs: '))
+for i in range(0, quantidade):
+  valorcd = int(input('Valor do CD: '))
+  cds.append(valorcd)
+  
+valortotal = sum(cds)
+valormedio = valortotal/quantidade
+print('Valor Médio: ', valormedio, 'Valor total: ', valortotal) """
+
+##Notas.py
+""" alunos = [] 
+for i in range(5):
+  nota1 = eval(input('Nota 1: '))
+  nota2 = eval(input('Nota 2: '))
+  nota3 = eval(input('Nota 3: '))
+  nota4 = eval(input('Nota 4: '))
+
+qtdfaltas = eval(input('Quantidade de faltas: '))
+media =(nota1*2+nota2*3+nota3*1+nota4*4)/10
+faltas = qtdfaltas*0.01
+
+if media>6 and faltas<25:
+  print('Aprovado com média', media)
+  nome = input('Nome do aluno: ')
+  alunos.append(nome)
+elif media<3 or faltas>25:
+    print('Reprovado')
+print(alunos) """
