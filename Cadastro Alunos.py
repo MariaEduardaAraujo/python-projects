a941b5480a3afd54dfe5423dc563af12fd2e5d50
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 20:42:00 -0300

    Update Cadastro Alunos.py

--- Cadastro Alunos.py
+++ Cadastro Alunos.py
@@ -1,4 +1,5 @@
-nomesAlunos = []
+## Cadastro Alunos 2
+""" nomesAlunos = []
 print('''   Menu
             1- Cadastrar
             2 - Gravar
@@ -20,6 +21,32 @@ while opcao!=3:
         cadastro()
     elif opcao==2:
         gravar()
-    opcao = int(input('Opção: '))
+    opcao = int(input('Opção: ')) """
 
 #Rodar este programa na IDLE do Phyton
+
+## Cadastro Alunos 2
+""" nomesAlunos = []
+print('''   Menu
+            1- Cadastrar
+            2 - Gravar
+            3 - Sair''')
+
+opcao = int(input('Opção: '))
+
+def cadastro():
+    nome = input('Nome: ')
+    
+def gravar():
+    arq = open('nome.txt', 'r+')
+    nomesAlunos.append('Lista Nomes\n')
+    nomesAlunos.append('--------------\n')
+    arq.writeline(nomesAlunos)
+    arq.close()
+
+while opcao!=3:
+    if opcao==1:
+        cadastro()
+    elif opcao==2:
+        gravar()
+    opcao = int(input('Opção: ')) """
