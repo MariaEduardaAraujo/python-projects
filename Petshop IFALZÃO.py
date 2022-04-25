c49e12b06f68bf89cd77c2f9fd06a21af771e8c4
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-04-25 19:36:01 -0300

    Update Petshop IFALZÃO.py

--- Petshop IFALZÃO.py
+++ Petshop IFALZÃO.py
@@ -8,10 +8,10 @@ bt = 75
 soma = []
 
 dono = input('Nome do cliente: ')
-tipoanima = input("Tipo do animal: ")
-nomeanimal = input('Nome do animal: ')
+tipo = input("Tipo do animal: ")
+nome = input('Nome do animal: ')
 cliente.append(dono)
-nomedoanimal.append(nomeanimal)
+nomedoanimal.append(nome)
 
 print('''Pet Shop IFALZÃO
             1- Servirços:
@@ -19,6 +19,7 @@ print('''Pet Shop IFALZÃO
                 1.3 - Tosa
                 1.4 - Banho e tosa
                 1.5 - Veterinário
+                1.6 - Valor Total
             2 - Sair''')
 
 opcao = eval(input('Opção desejada: '))
@@ -27,29 +28,31 @@ def servico():
     serv = eval(input('Serviço desejado: '))
     if serv == 1.2:
         opb = banho
-        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nomedoanimal , 'classificado como: ', tipoanima)
+        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nome , 'classificado como: ', tipo)
         soma.append(opb)
         print("preço a pagar: ",soma)
     elif serv == 1.3:
         opt = tosa
-        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nomedoanimal , 'classificado como: ', tipoanima)
+        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nome , 'classificado como: ', tipo)
         soma.append(opt)
         print("preço a pagar: ", soma)
     elif serv == 1.4:
         opbt = bt
-        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nomedoanimal , 'classificado como: ', tipoanima)
+        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nome , 'classificado como: ', tipo)
         soma.append(opbt)
         print("preço a pagar: ", soma)
     elif serv == 1.5:
         opv = veterinario
-        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nomedoanimal , 'classificado como: ', tipoanima)
+        print('Sr.(a)', cliente, 'seu animal de estimação é: ', nome , 'classificado como: ', tipo)
         soma.append(opv)
         print("preço a pagar: ", soma)
+    elif serv == 1.6:
+       print('Obrigado por usar nossos serviços! Até mais!')
+       print('Total gasto: ', sum(soma))
 
 while opcao!= 2:
     if opcao==1:
         servico()
     else:
         print('Obrigado por usar nossos serviços! Até mais!')
-        print('Total gasto: ', sum(soma))
     opcao = eval(input('Opção desejada: '))
