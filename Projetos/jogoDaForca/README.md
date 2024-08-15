2f8464b42bef75092e963e1ef2bed4f32d9a2642
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-08-15 20:33:43 -0300

    Arquivos atualizados para a versão 2 do jogo da forca

--- Projetos/jogoDaForca/README.md
+++ Projetos/jogoDaForca/README.md
@@ -1,6 +1,8 @@
-# Jogo da Forca dos Países nas Olimpíadas V1 🥇✨
+# Jogo da Forca dos Países nas Olimpíadas 🥇✨
 
 Bem-vindo ao "Jogo da Forca dos Países nas Olimpíadas"! Aproveitando o clima de Olimpíadas e Paralimpíadas, este é um simples jogo da forca onde o objetivo é adivinhar o nome de um dos países no TOP 20 do quadro de medalhas 2024. Você terá 6* chances para acertar a palavra secreta antes que a forca esteja completa.
+
+*Essas chances mudam conforme o nível selecionado.
 
 ## Descrição 📝
 
@@ -11,7 +13,19 @@ O jogo escolhe aleatoriamente um país da lista e você deve tentar adivinhar o 
 1. Clone o repositório ou copie o código para um arquivo `.py`.
 2. Execute o código usando Python: `python jogoDaForca.py`.
 
-## Contribuição
+## Versões 🔤
+**V1**
+1. **Início do Jogo:** O jogo começa solicitando que você escolha se deseja participar.
+2. **Tentativas:** Você tem 6 tentativas para adivinhar o país secreto.
+3. **Adivinhação:** Insira um palpite e veja se está correto.
+4. **Forca:** A forca é desenhada progressivamente com base no número de tentativas restantes.
+5. **Resultado:** O jogo termina quando você adivinha a palavra correta ou perde todas as suas chances.
+
+**V2**
+1. **Palpite dado:** As letras corretas no palpite são exibidas. 
+2. **Nível de dificuldade:** É possível selecionar o nível de dificuldade que vai do 1 (fácil) até o 3 (difícil), tendo o número de chances reduzidos a cada nível.
+
+## Contribuição 🤝
 
 Se você tiver sugestões ou encontrar problemas, sinta-se à vontade para abrir uma issue ou contribuir com um pull request.
 
--- Projetos/jogoDaForca/jogoDaForca.py
+++ Projetos/jogoDaForca/jogoDaForca.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 def forca(tentativas):
     estados = [
@@ -8,7 +9,6 @@ def forca(tentativas):
          O   |
         /|\\  |
         / \\  |
-             |
         ----------
         """,
         """
@@ -17,7 +17,6 @@ def forca(tentativas):
          O   |
         /|\\  |
         /    |
-             |
         ----------
         """,
         """
@@ -25,7 +24,6 @@ def forca(tentativas):
          |   |
          O   |
         /|\\  |
-             |
              |
         ----------
         """,
@@ -35,7 +33,6 @@ def forca(tentativas):
          O   |
         /|   |
              |
-             |
         ----------
         """,
         """
@@ -43,7 +40,6 @@ def forca(tentativas):
          |   |
          O   |
          |   |
-             |
              |
         ----------
         """,
@@ -53,13 +49,11 @@ def forca(tentativas):
          O   |
              |
              |
-             |
         ----------
         """,
         """
          -----
          |   |
-             |
              |
              |
              |
@@ -69,6 +63,17 @@ def forca(tentativas):
     
     print(estados[tentativas])
 
+def letras():
+    palavra = " ".join(paisSecreto)
+    letraAdv = " "
+    for l in alfabeto:
+        for letra in palavra:
+            if (l == letra):
+                if (letra in chutePalavra):
+                    letraAdv += letra
+    print("As seguintes letras estão na palavra: ", " ".join(letraAdv))
+    print()
+            
 
 print("Seja bem-vind@ ao...")
 print("Jogo da Forca dos Países nas Olimpíadas!")
@@ -80,6 +85,7 @@ paises = ["Estados Unidos","China","Japão","Austrália","França","Países Baix
 
 random.shuffle(paises)
 paisSecreto = (paises[0]).upper()
+alfabeto = list(string.ascii_uppercase)
 
 print("Você terá 6 chances de acertar o país secreto")
 print("E atingirá a forca caso perca todas as suas chances!")
@@ -91,7 +97,20 @@ print()
 
 try:
     if (opcao == "S" or opcao == "SIM"):
-        chances = 6
+        nivel = int(input("Selecione o nível de dificuldade: [1/2/3] "))
+        if (nivel == 1):
+            chances = 6
+            print(f"Você tem {chances} chances para tentar acertar a palavra")
+            print()
+        elif (nivel == 2):
+            chances = 4
+            print(f"Você tem {chances} chances para tentar acertar a palavra")
+            print()
+        else:
+            chances = 3
+            print(f"Você tem {chances} chances para tentar acertar a palavra")
+            print()
+
         while (chances != 0):
             for i in range(len(paisSecreto)):
                 print("_", end=" ")
@@ -101,15 +120,18 @@ try:
             chutePalavra.strip()
 
             if (chutePalavra == paisSecreto):
-                    print("Parabens! Você acertou!!!")
+                    print("Parabéns! Você acertou!!!")
                     break
             else:
                 if (chutePalavra != paisSecreto):
                     print("Que pena, essa não é a palavra secreta! Tente novamente!")
-                    print(f"Você tem: {chances - 1}")
+                    print(f"Você tem: {chances - 1} chances")
                     print()
+
                     chances-=1
                     forca(chances)
+                    letras()
+
         if(chances == 0):
             print("Que pena, você perdeu!")
             print(f"A palavra era: {paisSecreto}")
